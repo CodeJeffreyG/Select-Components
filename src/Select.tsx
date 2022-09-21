2d2d99363ce4162e8a3ff05fcdabd7090439916f
Jeffrey <jeffrey.graham600@gmail.com>
2022-09-21 08:37:34 -0400

    created typescript interface for select options

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,5 +1,12 @@
+type selectOptions = {
+   label: string,
+   value: string 
+}
+
 type SelectProps = {
-    
+    options:selectOptions[]
+    value?: selectOptions | undefined
+    onChange: (value: selectOptions | undefined) => void
 }
 
 export default function Select({value, onChange, options} : SelectProps)
